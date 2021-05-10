778917fe2cf912f926d00014241f279d8b61f90e
Rocc  Colonna <colonna.rocco2000@gmail.com>
2021-05-10 17:25:57 +0200

    nav bar

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,15 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    menuLink: ["Home", "Pages", "Program", "Tickets", "Papers", "Blog", "Shortcodes"],
+    activeMenuLink: 0
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,15 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            menuLink: [ "Home", "Pages", "Program", "Tickets", "Papers", "Blog", "Shortcodes" ],
+            activeMenuLink : 0,
+        },
+        methods: {
+
+        },
+        mounted () {
+
+        }
+    }
+)
