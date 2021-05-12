e9754efcf872afb9e22430eb2d2e95630db09b89
Rocc  Colonna <colonna.rocco2000@gmail.com>
2021-05-12 15:43:48 +0200

    finito main

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,9 @@ var app = new Vue({
   el: '#root',
   data: {
     menuLink: ["Home", "Pages", "Program", "Tickets", "Papers", "Blog", "Shortcodes"],
-    activeMenuLink: 0
+    activeMenuLink: 0,
+    imgSponsors: ["1", "2", "3", "4", "5", "6"],
+    activeImgSponsors: 0
   },
   methods: {},
   mounted: function mounted() {}
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ var app = new Vue(
         data: {
             menuLink: [ "Home", "Pages", "Program", "Tickets", "Papers", "Blog", "Shortcodes" ],
             activeMenuLink : 0,
+            imgSponsors:[ "1", "2", "3", "4", "5", "6" ],
+            activeImgSponsors: 0,
         },
         methods: {
 
